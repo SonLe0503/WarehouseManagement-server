c47aa541dac50b014f49f82644fa0514e872bda1
ThangLM <thanglm240723@gmail.com>
2026-02-04 16:29:39 +0700

    Inbound page of purchase v1

---
+++ DTOs/InBoundRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace warehouseManagement.DTOs
+{
+    public class InBoundRequest
+    {
+        public int InBoundRequestId { get; set; }
+        public  string SupplierName { get; set; }
+        public int InBoundWarehouseId { get; set; }
+        public DateTime CreateAt { get; set; }
+        public int CreatedBy { get; set; }
+
+
+    }
+}
