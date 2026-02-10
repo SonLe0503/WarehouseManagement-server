7958b391e03b8c97e853851b11b2a6c65dc1eb04
ThangLM <thanglm240723@gmail.com>
2026-02-10 23:02:33 +0700

    purchase page , create , update , delete, detail

--- Program.cs
+++ Program.cs
@@ -65,7 +65,7 @@ var app = builder.Build();
 app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.MapControllers();
 app.UseSwagger();
 app.UseSwaggerUI();
