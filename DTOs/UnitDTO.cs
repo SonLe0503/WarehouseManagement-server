4ed246b05d39d882c18ac5e67bbbeb4efd3c3c1a
SonLe0503 <SonLVHE172736@fpt.edu.vn>
2026-02-10 23:04:51 +0700

    [Update] product and unit

--- DTOs/UnitDTO.cs
+++ DTOs/UnitDTO.cs
@@ -1,6 +1,10 @@
 ﻿namespace warehouseManagement.DTOs
 {
-    public class Unit
+    public class UnitDTO
     {
+        public string Code { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public bool IsBaseUnit { get; set; }
     }
 }
