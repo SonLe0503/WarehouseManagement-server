16ad783446e5330f56ac4adf1c17c9db41fc3c4e
SonLe0503 <SonLVHE172736@fpt.edu.vn>
2026-02-13 23:11:08 +0700

    [Add] crud inventories

---
+++ Controllers/InventoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace warehouseManagement.Controllers
+{
+    public class InventoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
