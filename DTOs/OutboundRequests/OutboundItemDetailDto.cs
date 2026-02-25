6ef2bd7e42176d4f2d8caaed11434be47d795914
SonLe0503 <SonLVHE172736@fpt.edu.vn>
2026-02-25 16:12:55 +0700

    [Add] outbound

---
+++ DTOs/OutboundRequests/OutboundItemDetailDto.cs
@@ -0,0 +1,11 @@
+﻿namespace warehouseManagement.DTOs.OutboundRequests
+{
+    public class OutboundItemDetailDto
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public decimal Quantity { get; set; }
+        public string? LineNote { get; set; }
+        public ProductDTO? Product { get; set; }
+    }
+}
---
+++ DTOs/OutboundRequests/OutboundRequestItemCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace warehouseManagement.DTOs.OutboundRequests
+{
+    public class OutboundRequestItemCreateDto
+    {
+        public int ProductId { get; set; }
+        public decimal Quantity { get; set; }
+        public string? LineNote { get; set; }
+    }
+}
