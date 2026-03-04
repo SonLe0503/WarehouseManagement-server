68e3894b2fb6ae748e284b91b9287c123c2f8ed4
phuongnq123 <qp572492@gmail.com>
2026-03-04 14:56:09 +0700

    [Update] picked

---
+++ DTOs/PickedOutboundItemDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace warehouseManagement.DTOs
+{
+    public class PickedInboundRequestDTO
+    {
+        public List<PickedInboundItemDTO> Items { get; set; } = new();
+    }
+
+    public class PickedInboundItemDTO
+    {
+        public int OutboundItemId { get; set; }
+        public decimal PickedQuantity { get; set; }
+        public string? StoragePosition { get; set; }
+        public string? LineNote { get; set; }
+    }
+}
