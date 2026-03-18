013ee4d4baf9f6134bdd1d858e025a88e156d7b5
SonLe0503 <sonlvhe172736@fpt.edu.vn>
2026-03-18 22:47:16 +0700

    [Update] migrations

--- Migrations/20260310104237_FixInventoryUniqueConstraint.cs
+++ Migrations/20260310104237_FixInventoryUniqueConstraint.cs
@@ -10,30 +10,30 @@ namespace warehouseManagement.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "UQ_Product_Warehouse",
-                table: "Inventories");
+            //migrationBuilder.DropIndex(
+            //    name: "UQ_Product_Warehouse",
+            //    table: "Inventories");
 
-            migrationBuilder.CreateIndex(
-                name: "UQ_Product_Warehouse_Bin",
-                table: "Inventories",
-                columns: new[] { "ProductId", "WarehouseId", "StoragePosition" },
-                unique: true,
-                filter: "[StoragePosition] IS NOT NULL");
+            //migrationBuilder.CreateIndex(
+            //    name: "UQ_Product_Warehouse_Bin",
+            //    table: "Inventories",
+            //    columns: new[] { "ProductId", "WarehouseId", "StoragePosition" },
+            //    unique: true,
+            //    filter: "[StoragePosition] IS NOT NULL");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "UQ_Product_Warehouse_Bin",
-                table: "Inventories");
+            //migrationBuilder.DropIndex(
+            //    name: "UQ_Product_Warehouse_Bin",
+            //    table: "Inventories");
 
-            migrationBuilder.CreateIndex(
-                name: "UQ_Product_Warehouse",
-                table: "Inventories",
-                columns: new[] { "ProductId", "WarehouseId" },
-                unique: true);
+            //migrationBuilder.CreateIndex(
+            //    name: "UQ_Product_Warehouse",
+            //    table: "Inventories",
+            //    columns: new[] { "ProductId", "WarehouseId" },
+            //    unique: true);
         }
     }
 }
